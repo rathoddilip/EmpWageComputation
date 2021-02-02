bea621e344d9ed8287d0281f4f48070f2f9713f6
diliprathod32 <diliprathod32@gmail.com>
2021-02-03 01:44:17 +0530

    [Dilip] UseCaseStament

--- Program.cs
+++ Program.cs
@@ -13,11 +13,17 @@ namespace EmpWage
             int empDailyHr = 0;
             int perDaySalary = 0;
             Random randomNumber = new Random();
-            int empCheck = randomNumber.Next(0, 2);
+            int empCheck = randomNumber.Next(0, 3);
             Console.WriteLine(empCheck);
 			switch ((int)empCheck)
 			{
 				case 1:
+					empDailyHr = 10;
+					Console.WriteLine("Full time Employee");
+					perDaySalary = empRatePerHr * empDailyHr;
+					Console.WriteLine("Employee per day salary = " + perDaySalary);
+					break;
+				case 2:
 					empDailyHr = 8;
 					Console.WriteLine("Part time Employee");
 					perDaySalary = empRatePerHr * empDailyHr;
