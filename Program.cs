f7fe9125229521c802d7035bd21247a35cb74291
diliprathod32 <diliprathod32@gmail.com>
2021-02-02 23:51:28 +0530

    [Dilip] WelcomeToEmpWageComputation

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage computation");
+              
+        }
+    }
+}
