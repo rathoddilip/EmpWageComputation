d9d69b25cd0adc751174c29b2c29c0425ad54d6f
diliprathod32 <diliprathod32@gmail.com>
2021-02-03 01:03:36 +0530

    [Dilip] DailyEmpWageSalary

--- Program.cs
+++ Program.cs
@@ -8,12 +8,17 @@ namespace EmpWage
         {
             Console.WriteLine("Welcome to Employee Wage computation");
             int isPresent = 1;
+            int empRatePerHr = 20;
+            int empDailyHr = 8;
             Random randomNumber = new Random();
             int empCheck = randomNumber.Next(0, 2);
             Console.WriteLine(empCheck);
             if (empCheck == isPresent)
+            {
                 Console.WriteLine("present");
-
+                int perDaySalary = empRatePerHr * empDailyHr;
+                Console.WriteLine("Employee Per Day salary = " + perDaySalary);
+            }
             else
                 Console.WriteLine("absent");
         }
